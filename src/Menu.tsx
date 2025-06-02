d70251e98352d7566bfaafd70396f3723bfc0e7d
cyr-ius <cyr-ius@ipocus.net>
2025-06-02 17:56:33 +0000

    Add translate strings

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,8 +1,8 @@
-import MenuIcon from '@mui/icons-material/Menu';
-import { Box, IconButton, Menu, MenuItem } from '@mui/material';
+import MenuIcon from "@mui/icons-material/Menu";
+import { Box, IconButton, Menu, MenuItem } from "@mui/material";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-
 
 export const MenuDropdown = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -11,43 +11,46 @@ export const MenuDropdown = () => {
     setAnchorEl(event.currentTarget);
   };
   const open = Boolean(anchorEl);
+  const { t } = useTranslation();
 
   const handleClose = () => {
     setAnchorEl(null);
   };
 
   return (
-
-        <Box position="absolute" top={10} left={10}>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                onClick={handleClick}>
-                <MenuIcon />
-            </IconButton>
-            <Menu
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
-                <MenuItem
-                    onClick={() => {
-                        navigate('/incident');
-                        handleClose();
-                    } }>
-                    Incident Report
-                </MenuItem>
-                <MenuItem
-                    onClick={() => {
-                        navigate('/threat');
-                        handleClose();
-                    } }>
-                    Threat Report
-                </MenuItem>
-            </Menu>
-        </Box>
-    )
-
-}
+    <Box position="absolute" top={10} left={10}>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        onClick={handleClick}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+      >
+        <MenuItem
+          onClick={() => {
+            navigate("/incident");
+            handleClose();
+          }}
+        >
+          {t("menu.incidentReport", "Incident Report")}
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            navigate("/threat");
+            handleClose();
+          }}
+        >
+          {t("menu.threatReport", "Threat Report")}
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
