a342132a9c5d0c42fd463911726db2c499f4ac55
cyr-ius <cyr-ius@ipocus.net>
2025-06-02 17:56:24 +0000

    Add translate strings

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,9 @@
-import { Grid } from '@mui/material';
-import { type FC } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useLocation } from 'react-router-dom';
-import { useDebug } from './contexts/DebugContext';
-import { MenuDropdown } from './Menu';
+import { Grid } from "@mui/material";
+import { type FC } from "react";
+import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
+import { useDebug } from "./contexts/DebugContext";
+import { MenuDropdown } from "./Menu";
 
 export const Header: FC = () => {
   const { debugMode, toggleDebugMode } = useDebug();
@@ -14,19 +14,26 @@ export const Header: FC = () => {
     <Grid className="App">
       <header className="App-header">
         <MenuDropdown />
-        <img src="./logo.svg" className="Header-logo" alt="logo" onClick={toggleDebugMode}/>
+        <img
+          src="./logo.svg"
+          className="Header-logo"
+          alt="logo"
+          onClick={toggleDebugMode}
+        />
 
-        {(currentPath.pathname == '/incident' ||
-          currentPath.pathname == '/') && (
-          <h1 className="title">{t('DORA Major Incident Report')}</h1>
+        {(currentPath.pathname == "/incident" ||
+          currentPath.pathname == "/") && (
+          <h1 className="title">
+            {t("incident:title", "DORA Major Incident Report")}
+          </h1>
         )}
-        {currentPath.pathname == '/threat' && (
-          <h1 className="title">{t('DORA Threat Report')}</h1>
+        {currentPath.pathname == "/threat" && (
+          <h1 className="title">{t("threat:title", "DORA Threat Report")}</h1>
         )}
-        <p className="subtitle">{t('European Banking Authority')}</p>
+        <p className="subtitle">{t("European Banking Authority")}</p>
       </header>
       {debugMode && (
-        <div style={{ marginTop: '20px', color: 'red' }}>
+        <div style={{ marginTop: "20px", color: "red" }}>
           Debug mode is <strong>ON</strong>
         </div>
       )}
